51e7fbbdf7bdcad2290932dc4b0cef9092359d48
Kirill Vine <61331094+Kirill-Vine@users.noreply.github.com>
2022-10-06 17:32:49 -0400

    Create get_user_genre.py

---
+++ task4/get_user_genre.py
@@ -0,0 +1,29 @@
+def read_movie_genre(f): #1.2
+    dct = {}
+    prevmovie=''
+    for line in open(f):
+        if prevmovie!=line[line.index('|')+3:].strip('\n'):
+            movie = line[line.index('|')+3:].strip('\n')
+            genre = line[:line.index('|')]
+        dct[movie] = genre
+        prevmovie=movie
+    return dct
+def read_user_ratings(f): #4.1
+    dct = {}
+    for line in open(f):
+        if line[line.index('|')+5:].strip('\n') not in dct:
+            dct[line[line.index('|')+5:].strip('\n')] = list()
+        dct[line[line.index('|')+5:].strip('\n')].append(tuple((line[:line.index('|')],line[line.index('|')+1:line.index('|')+4])))
+    return dct 
+def get_user_genre(uid,f,e):
+    dct = f.get(str(uid))
+    lst=list()
+    movie=None
+    for i in dct:
+        lst.append(i[1])
+    for i in dct:
+        if i[1]==max(lst):
+            movie=i[0]
+    genre=e.get(str(movie))
+    return genre
+get_user_genre(43,read_user_ratings("movieRatingSample.txt"), read_movie_genre('genreMovieSample.txt'))
