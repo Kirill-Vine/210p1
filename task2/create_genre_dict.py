900ec081cd9f9ed597ae698d86e709e48534d718
Kirill Vine <61331094+Kirill-Vine@users.noreply.github.com>
2022-10-06 15:22:15 -0400

    Create create_genre_dict.py

---
+++ task2/create_genre_dict.py
@@ -0,0 +1,23 @@
+def read_ratings_data(f):
+    dct = {}
+    prevmovie=''
+    for line in open(f):
+        if prevmovie!=line[:line.index('|')]:
+            rating=[]
+        movie = line[:line.index('|')]
+        rating.append(line[line.index('|')+1:line.index('|')+4])
+        dct[movie]=rating
+        prevmovie=movie
+    return dct 
+def calculate_average_rating(d):
+    ans = {}
+    for(name,ratingList) in d.items():
+        sum = 0
+        count = 0
+        for rating in ratingList:
+            sum+=float(rating)
+            count+=1
+        average = float("{:.1f}".format(sum/count))
+        ans[name] = average
+    return ans
+calculate_average_rating(read_ratings_data("movieRatingSample.txt"))
