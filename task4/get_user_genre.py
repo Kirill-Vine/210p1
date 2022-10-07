13e662952af056ba6b2ca5f0666c91dba56cae79
Kirill Vine <61331094+Kirill-Vine@users.noreply.github.com>
2022-10-06 20:15:12 -0400

    Update get_user_genre.py

--- task4/get_user_genre.py
+++ task4/get_user_genre.py
@@ -3,7 +3,7 @@ def read_movie_genre(f): #1.2
     prevmovie=''
     for line in open(f):
         if prevmovie!=line[line.index('|')+3:].strip('\n'):
-            movie = line[line.index('|')+3:].strip('\n')
+            movie = line[line.index('|')+3:].strip('\n|')
             genre = line[:line.index('|')]
         dct[movie] = genre
         prevmovie=movie
